38a5e78afc346c4b602f9c8d3e67b0cbe749acc0
henglihengli <lsliheng@gmail.com>
2020-09-05 10:39:12 -0700

    TinNews home view

--- app/src/main/java/com/laioffer/tinnews/ui/search/SearchFragment.java
+++ app/src/main/java/com/laioffer/tinnews/ui/search/SearchFragment.java
@@ -26,7 +26,6 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         binding = FragmentSearchBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
